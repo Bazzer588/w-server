c523e7c57fac29ec7544518d1ac55b49c058eda7
Bazzer588 <40038437+Bazzer588@users.noreply.github.com>
2019-03-06 22:28:58 +0000

    Update random.js

--- src/random.js
+++ src/random.js
@@ -1,4 +1,9 @@
 
 export function random (req, res) {
-    res.send({ random: Math.random() });
+    const random = Math.random();
+    console.log('RANDOM',random,req.query);
+    if (req.query.foo) {
+        throw(new Error('You said foo'));
+    }
+    res.send({ random, reqQuery: req.query });
 }
