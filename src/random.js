23d6393c5884883a58e3269a24a5f73791d9508a
Bazzer588 <40038437+Bazzer588@users.noreply.github.com>
2019-03-10 01:43:08 +0000

    Added environment variable handling

--- src/random.js
+++ src/random.js
@@ -1,22 +1,29 @@
 
-export function random (req, res) {
+const FOO_BAR = process.env.FOO_BAR;   // injected at run time, ie set FOO_BAR=1234567 && node server
+const WOOT = process.env.WOOT;         // injected at build time by plugin
+const NODE_ENV = process.env.NODE_ENV; // injected at build
+
+export const random = (req, res) => {
     const random = Math.random();
-    console.log('RANDOM',random,req.query);
+    console.log('RANDOM',random,req.query,'FOO_BAR',FOO_BAR,'MAGIC',MAGIC_DEFINE,'WOOT',WOOT);
     if (req.query.foo) {
         throw(new Error('You said foo'));
     }
-    const T = new AThing();
-    res.send({ random, reqQuery: req.query, ...T.getv() });
-}
+    const T = new AThing(80837);
+    res.send({ random, reqQuery: req.query, ...T.getv(), FOO_BAR, NODE_ENV });
+};
+
+// example class
 
 class AThing {
 
-    constructor () {
+    constructor (v) {
+        this.v = v;
         this.When = Date.now();
     }
 
     getv () {
-        return { when: this.When };
+        return { when: this.When, v: this.v };
     }
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,33 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  target: 'node',
-  mode: 'production',
-  devtool: 'source-map',
-  entry: './src/index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'server')
-  },
-  externals: [
-    // 'express' // see note on Critical dependency: the request of a dependency is an expression
-  ]
-  // exprContextCritical: false  // supposed to get rid of the warning, but actually provokes an error !
+module.exports = (env) => {
+
+    console.log('BUILD ENV', env);
+
+    return {
+        target: 'node',
+        mode: 'production',
+        devtool: 'source-map',
+        entry: './src/index.js',
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'server')
+        },
+        externals: [
+            // 'express' // see note on Critical dependency: the request of a dependency is an expression
+        ],
+        module: {
+            exprContextCritical: false  // prevents the warning described below
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                MAGIC_DEFINE: JSON.stringify('8324783')
+                //'process.env.WOOT': JSON.stringify(process.env.WOOT)
+            }),
+            new webpack.EnvironmentPlugin(['WOOT'])
+        ]
+    };
 };
 
 /* On build we get a WARNING: Critical dependency: the request of a dependency is an expression
@@ -21,5 +36,4 @@ module.exports = {
    Which is no good, as it doesn't require express...
    
    Otherwise, we get a complete js build in /server/index.js (about 544 KB)
-
 */
